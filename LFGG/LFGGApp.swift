bb809fed16f4f4c73000e3bad6a4e26324105bbc
kim-iths <kim.hellman@iths.se>
2021-02-06 23:47:02 +0100

    Initial Commit

---
+++ LFGG/LFGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  LFGGApp.swift
+//  LFGG
+//
+//  Created by Kim Hellman on 2021-02-06.
+//
+
+import SwiftUI
+
+@main
+struct LFGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
